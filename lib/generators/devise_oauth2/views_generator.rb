2a978652e5d09d196d6420d4676168363a21240f
Brian Wheeler <bwheeler96@gmail.com>
2014-01-24 07:51:22 -0800

    test view generator

--- lib/generators/devise_oauth2/views_generator.rb
+++ lib/generators/devise_oauth2/views_generator.rb
@@ -1,6 +1,6 @@
 class ViewsGenerator < Rails::Generators::Base
 	
-	File.open(File.expand_path('../../../../app/views/devise_outh/authorize.html.haml'), 'w+') do f
+	File.open(File.expand_path('../../../../app/views/devise_outh/authorize.html.haml', __FILE__), 'w+') do f
 		f << 'helo'
 	end
 
