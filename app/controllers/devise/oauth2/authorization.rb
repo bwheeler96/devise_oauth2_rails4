41ed0dd76aef28354288c0d4fd16b6251c328c08
Brian Wheeler <bwheeler96@gmail.com>
2014-03-14 17:07:47 -0700

    auth layer fix and caching

--- app/controllers/devise/oauth2/authorization.rb
+++ app/controllers/devise/oauth2/authorization.rb
@@ -5,13 +5,17 @@ module Devise
 
       def current_oauth2_client(opts: { secure: false })
         return @secure_client ||= Client.find_by(secret: client_secret) if opts[:secure]
-        return @secure_client ||= Client.find_by(id: current_access_token.client_id) if access_token?
+        return @secure_client ||= Client.find_by(id: current_access_token.client_id) if valid_access_token?
         @client ||= Client.find_by('identifier = :client_id OR secret = :secret', client_id: client_id, secret: client_secret) if client_credentials?
       end
 
       def current_access_token
-        @access_token ||= AccessToken.find_by(token: access_token)
+        @access_token ||= AccessToken.find_by(token: access_token) if accesss_token?
       end
+
+			def valid_access_token?
+				!!current_access_token
+			end
 
       def oauth2_client_signed_in?
         !!@client
